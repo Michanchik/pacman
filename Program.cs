050cb0af6399a88869e98acde6bbdb3216f5cff0
Марина Л <Marina@MacBook-Air-Marina.local>
2024-02-05 08:05:10 +0300

    первая фиксация

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Runtime.CompilerServices;
+
+
+
+namespace CSharp
+{
+
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            char[,] map = ReadMap("map.txt");
+
+            while (true)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                DrawMap(map);
+
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+                Console.SetCursorPosition(1, 1);
+                Console.Write("@");
+
+                Thread.Sleep(1000);
+            } 
+
+    
+        }
+
+        private static char[,] ReadMap(string path)
+        {
+            string[] file = File.ReadAllLines("map.txt");
+
+            char[,] map = new char[file[0].Length, file.Length];
+
+            for(int x = 0; x < map.GetLength(0); x++)
+                for(int y = 0; y < map.GetLength(0); y++)
+                    map[x, y] = file[x][y];
+
+            return map;
+        
+        } 
+
+
+        private static void DrawMap(char[,] map)
+        {   
+            for(int y = 0; y < map.GetLength(0); y++)
+                for(int x = 0; x < map.GetLength(0); x++)
+                {                    
+                    Console.Write(map [x, y]);
+                }
+                Console.WriteLine();
+        }  
+    }
+
+}
+
+
+
